1856baf466bb8fe7ea34f1a81be0cc4e0d68a777
Anna Vognstoft Mikkelsen <anmi9@student.eadania.dk>
2026-05-18 14:54:23 +0200

    src(avm): Implementering af sekvensdiagram 1

---
+++ src/Models/BookingRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Models
+{
+    internal class BookingRepository
+    {
+
+        public IEnumerable<Vehicle> GetVehicles(Booking booking)
+        {
+            //Request available Vehicles from DB
+            return default;
+        }
+    }
+}
---
+++ src/Models/BookingService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Models
+{
+    internal class BookingService
+    {
+
+        public BookingService()
+        {
+            Booking booking = new Booking();
+
+            //Dummy data for testing
+            var start = new DateTime(2024, 6, 1, 8, 0, 0);
+            var end = new DateTime(2024, 6, 1, 17, 0, 0);
+            var Employee = 1;
+
+            booking.Start = start;
+            booking.End = end;
+            booking.EmployeeId = Employee;
+
+            BookingRepository bookingRepository = new BookingRepository();
+            bookingRepository.GetVehicles(booking);
+
+
+        }
+
+
+
+
+    }
+}
