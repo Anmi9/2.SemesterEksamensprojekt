c31af36e7935e529b909f9e3baaaee1b8300e9d1
Lasse Agerskov <lasse.agerskov@gmail.com>
2026-05-20 19:24:48 +0200

    src(lka): context til BookingRepo

--- src/Data/Repositories/BookingRepository.cs
+++ src/Data/Repositories/BookingRepository.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using App.Models;
+using SQLitePCL;
 
 namespace App.Data.Repositories
 {
     public class BookingRepository
     {
+        private readonly Context _context;
+        public BookingRepository(Context context) //Constructor der tager context med, så vi kan sikre at vi har adgang til databasen når BookingRepository oprettes.
+        {
+            _context = context;
+        }
+
         public void DBCreate(Booking booking)
         {
-            throw new NotImplementedException();
-            // connection til DB
-            // SQL Create 
+            _context.Bookings.Add(booking); //Tilføjer den til databasen, men EFcore holder det kun i "hukommelsen"
+            _context.SaveChanges(); //Objektet oversættes til SQL (INSERT INTO Bookings (VehiID, start, end) VALUES (.., ..., ..,) og EFcore lukker selv adgangen
+            // TODO: opret context i root og send dem til repos. (App.xaml.cs spørgsmålstegn
         }
     }
 }
