13f0c909e43e1e9f50648ffa9d7ba2393bc1c87f
Lasse Agerskov <lasse.agerskov@gmail.com>
2026-05-20 22:32:58 +0200

    src(lka): Forslag til DI Root

--- WPF/App.xaml.cs
+++ WPF/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using App.Data.Repositories;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +10,14 @@ namespace WPF
     /// </summary>
     public partial class App : Application
     {
+        //Composition root, Constructor injection, Dependency injection, Dependency Inversion Principle, SOLID
+       // protected override void OnStartup(startupeventblalalala e)
+       // base.OnStartup(e);
+       // var context = new Context();
+       // var bookingrepo = new BookingRepository(context);
+       //var vehiclerepo = new VehchileRepository(context);
+       // var mainWindow = new MainWindow(bookingrepo, vehiclerepo);
+       // mainWindow.Show();
     }
 
 }
--- src/Data/Repositories/BookingRepository.cs
+++ src/Data/Repositories/BookingRepository.cs
@@ -9,16 +9,16 @@ namespace App.Data.Repositories
     public class BookingRepository
     {
         private readonly Context _context;
-        public BookingRepository(Context context) //Constructor der tager context med, så vi kan sikre at vi har adgang til databasen når BookingRepository oprettes.
-        {
-            _context = context;
-        }
+       // public BookingRepository(Context context) //Constructor der tager context med, så vi kan sikre at vi har adgang til databasen når BookingRepository oprettes.
+       // {
+       //    _context = context;
+       // }
 
         public void DBCreate(Booking booking)
         {
             _context.Bookings.Add(booking); //Tilføjer den til databasen, men EFcore holder det kun i "hukommelsen"
             _context.SaveChanges(); //Objektet oversættes til SQL (INSERT INTO Bookings (VehiID, start, end) VALUES (.., ..., ..,) og EFcore lukker selv adgangen
-            // TODO: opret context i root og send dem til repos. (App.xaml.cs spørgsmålstegn
+            // TODO: opret context i root og send dem til repos. (App.xaml.spørgsmålstegn
         }
     }
 }
