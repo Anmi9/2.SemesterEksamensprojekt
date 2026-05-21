b166d2c3986e452fdae1794ee9bd0b79d0b320df
Matias Heiberg Nielsen <perm_diners7z@icloud.com>
2026-05-21 11:55:23 +0200

    WPF(mhn): tilføjet logik til dato og tids bokse

--- WPF/CreateBookingView.xaml.cs
+++ WPF/CreateBookingView.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using App.Models;
 
 namespace WPF
 {
@@ -24,12 +25,77 @@ namespace WPF
             InitializeComponent();
             _viewModel = new CreateBookingViewModel();
 
+            // Begræns DatePicker til at vise i dag og op til 4 uger (28 dage) frem
+            BookingDatePicker.DisplayDateStart = DateTime.Today;
+            BookingDatePicker.DisplayDate = DateTime.Today;
+            BookingDatePicker.DisplayDateEnd = DateTime.Today.AddDays(28);
+
+            // Sæt default valg til at være i dag
+            BookingDatePicker.SelectedDate = DateTime.Today;
+
+            InitializeTimeComboBoxes();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void InitializeTimeComboBoxes()
         {
-            throw new NotImplementedException();
-                // _viewModel.Type = App.Models.VehicleType.Car;
+            var times = new List<string>();
+            var startTime = new TimeSpan(0, 0, 0);
+            var endTime = new TimeSpan(23, 45, 0);
+            var interval = new TimeSpan(0, 15, 0);
+
+            var currentTime = startTime;
+            while (currentTime <= endTime)
+            {
+                // Brug @"HH\:mm" (eller string format med InvariantCulture) for at tvinge kolon (:) som separator (Da-DK kultur tvinger ellers punktum)
+                times.Add(new DateTime().Add(currentTime).ToString(@"HH\:mm"));
+                currentTime = currentTime.Add(interval);
+            }
+
+            StartTimeComboBox.ItemsSource = times;
+            EndTimeComboBox.ItemsSource = times;
+
+            UpdateTimeSelection();
+        }
+
+        private void BookingDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateTimeSelection();
+        }
+
+        private void UpdateTimeSelection()
+        {
+            if (BookingDatePicker.SelectedDate == DateTime.Today)
+            {
+                // Find nærmeste kvarter fremad (f.eks. hvis kl. er 14:10, vælg 14:15)
+                var now = DateTime.Now;
+                var minutes = now.Minute;
+                var adjustment = 15 - (minutes % 15);
+                var roundedTime = now.AddMinutes(adjustment);
+
+                string timeString = roundedTime.ToString(@"HH\:mm");
+
+                // Prøv at finde tidspunktet i comboboxen
+                var index = StartTimeComboBox.Items.IndexOf(timeString);
+                StartTimeComboBox.SelectedIndex = index;
+            }
+            else
+            {
+                // Hvis ikke det er i dag, sæt til 07:00
+                string defaultTime = "07:00";
+                var index = StartTimeComboBox.Items.IndexOf(defaultTime);
+                StartTimeComboBox.SelectedIndex = index;
+            }
+        }
+
+        // Time picker logik: DisplayTime er now hvis DateStar er Today(), ellers start på 07:00
+        private void BookCarClick(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Type = VehicleType.Car;
+        }
+
+        private void BookBikeClick(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Type = VehicleType.Bike;
         }
     }
 }
