9a067f0de89a2b4b50e0d9e33d86b4a424179a7d
Matias Heiberg Nielsen <perm_diners7z@icloud.com>
2026-05-28 10:38:53 +0200

    src(mhn): Opdateret Vehicle enum med flags attributen. Gjort kontrakten til Vehicle klassens properties stramme og robuste

--- src/Models/Vehicle.cs
+++ src/Models/Vehicle.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace App.Models
 {
-    public enum VehicleType
+    [Flags]
+    public enum VehicleTypes
     {
-        Car,
-        Bike,
+        None    = 0,
+        Car     = 1,
+        Bike    = 2
     }
 
     public class Vehicle
     {
-        public int VehicleId { get; set; }
-        public required VehicleType Type { get; set; } 
-        public required string LicensePlate { get; set; } 
+        public required int VehicleId { get; init; }
+        public required string LicensePlate { get; init; }
+        public required VehicleTypes Type
+        {
+            get;
+            init // Hvis køretøjet får tildelt mere end en type (aktive bits) fejler den
+            {
+                int activeBits = BitOperations.PopCount((uint)value); 
+                if (activeBits != 1)
+                {
+                    throw new ArgumentException($"Et specifikt køretøj må kun have præcis én type fra {nameof(VehicleTypes)} aktiveret.");
+                }
+                field = value;
+            }
+        }
 
     }
 }
